c5e1a9fe67b810118440bd053db68e0f77fad704
Turqay <turqayteymurov@gmail.com>
2024-12-11 18:03:46 +0400

    Form Guncelenmesi

--- WinFormsApp1/Form1.Designer.cs
+++ WinFormsApp1/Form1.Designer.cs
@@ -28,19 +28,69 @@
         /// </summary>
         private void InitializeComponent()
         {
+            txtWorkerName = new TextBox();
+            btnAddWorker = new Button();
+            lstWorkers = new ListBox();
+            btnDeleteWorker = new Button();
             SuspendLayout();
+            // 
+            // txtWorkerName
+            // 
+            txtWorkerName.Location = new Point(21, 38);
+            txtWorkerName.Name = "txtWorkerName";
+            txtWorkerName.Size = new Size(125, 27);
+            txtWorkerName.TabIndex = 0;
+            txtWorkerName.KeyDown += txtWorkerName_KeyDown;
+            // 
+            // btnAddWorker
+            // 
+            btnAddWorker.Location = new Point(21, 80);
+            btnAddWorker.Name = "btnAddWorker";
+            btnAddWorker.Size = new Size(94, 29);
+            btnAddWorker.TabIndex = 1;
+            btnAddWorker.Text = "Ekle";
+            btnAddWorker.UseVisualStyleBackColor = true;
+            btnAddWorker.Click += btnAddWorker_Click;
+            // 
+            // lstWorkers
+            // 
+            lstWorkers.FormattingEnabled = true;
+            lstWorkers.Location = new Point(205, 38);
+            lstWorkers.Name = "lstWorkers";
+            lstWorkers.Size = new Size(125, 344);
+            lstWorkers.TabIndex = 2;
+            // 
+            // btnDeleteWorker
+            // 
+            btnDeleteWorker.Location = new Point(336, 36);
+            btnDeleteWorker.Name = "btnDeleteWorker";
+            btnDeleteWorker.Size = new Size(94, 29);
+            btnDeleteWorker.TabIndex = 3;
+            btnDeleteWorker.Text = "Sil";
+            btnDeleteWorker.UseVisualStyleBackColor = true;
+            btnDeleteWorker.Click += btnDeleteWorker_Click;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(btnDeleteWorker);
+            Controls.Add(lstWorkers);
+            Controls.Add(btnAddWorker);
+            Controls.Add(txtWorkerName);
             Name = "Form1";
             Text = "Form1";
             Load += Form1_Load;
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private TextBox txtWorkerName;
+        private Button btnAddWorker;
+        private ListBox lstWorkers;
+        private Button btnDeleteWorker;
     }
 }
--- WinFormsApp1/Form1.cs
+++ WinFormsApp1/Form1.cs
@@ -2,14 +2,103 @@ namespace WinFormsApp1
 {
     public partial class Form1 : Form
     {
+        private string filePath = "workers.txt"; // Ýsimleri saklayacaðýmýz dosyanýn yolu
+
         public Form1()
         {
             InitializeComponent();
+            LoadWorkersFromFile(); // Uygulama baþlarken iþçileri yükle
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
 
         }
+
+        private void btnAddWorker_Click(object sender, EventArgs e)
+        {
+            AddWorker(); // Ýþçi ekleme iþlemini çaðýr
+           
+        }
+
+        private void SaveWorkerToFile(string workerName)
+        {
+            // Ýþçi ismini dosyaya ekle
+            File.AppendAllText(filePath, workerName + Environment.NewLine);
+        }
+
+        private void LoadWorkersFromFile()
+        {
+            if (File.Exists(filePath))
+            {
+                // Dosyadan tüm iþçi isimlerini oku
+                string[] workers = File.ReadAllLines(filePath);
+                lstWorkers.Items.AddRange(workers);
+            }
+
+        }
+
+        private void btnDeleteWorker_Click(object sender, EventArgs e)
+        {
+            if (lstWorkers.SelectedItem != null) // Seçili bir iþçi var mý kontrol et
+            {
+                // ListBox'tan seçilen iþçiyi al
+                string selectedWorker = lstWorkers.SelectedItem.ToString();
+
+                // ListBox'tan kaldýr
+                lstWorkers.Items.Remove(selectedWorker);
+
+                // Güncellenmiþ listeyi dosyaya kaydet
+                SaveAllWorkersToFile();
+            }
+            else
+            {
+                MessageBox.Show("Lütfen silmek için bir iþçi seçiniz.", "Uyarý", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void SaveAllWorkersToFile()
+        {
+            // Tüm iþçi isimlerini bir diziye aktar
+            string[] workers = new string[lstWorkers.Items.Count];
+            lstWorkers.Items.CopyTo(workers, 0);
+
+            // Dosyayý yeni liste ile güncelle
+            File.WriteAllLines(filePath, workers);
+        }
+
+        private void txtWorkerName_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter) // Enter tuþuna basýlmýþsa
+            {
+                // Ýþçi ekleme iþlemini çaðýr
+                AddWorker();
+
+                // Enter tuþunun baþka iþlemler yapmasýný engelle
+                e.Handled = true;
+                e.SuppressKeyPress = true;
+            }
+        }
+        private void AddWorker()
+        {
+            string workerName = txtWorkerName.Text.Trim();
+
+            if (!string.IsNullOrEmpty(workerName))
+            {
+                // Ýþçi ismini ListBox'a ekle
+                lstWorkers.Items.Add(workerName);
+
+                // Ýþçi ismini dosyaya kaydet
+                SaveWorkerToFile(workerName);
+
+                // TextBox'ý temizle
+                txtWorkerName.Clear();
+            }
+            else
+            {
+                MessageBox.Show("Lütfen bir iþçi ismi giriniz.", "Uyarý", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
     }
 }
